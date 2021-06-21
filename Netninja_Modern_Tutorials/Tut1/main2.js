ad54d1e53715fbb7cac8476672c71d92531533e1
Sachin2911 <Sachinmohan29@gmail.com>
2021-06-22 00:15:33 +0200

    Added

--- Netninja_Modern_Tutorials/Tut1/main2.js
+++ Netninja_Modern_Tutorials/Tut1/main2.js
@@ -5,5 +5,28 @@ let email = "Sachinmohan29@gmail.com";
 let result = email.lastIndexOf("a");
 console.log(result)
 
+//Slice opperator takes in 2 params =? starting and ending slice
 let result2 = email.slice(6, 11)
 console.log(result2)
+
+//Substring operator works sinilarly to slice, takes in 2 params first os the start and the 2nd instead of being the end is the number of chars it must go for. For example if start at postion 4 and set second parameter as 10 it will begin to slice at 4 and end at 14
+let result3 = email.substring(0,10)
+console.log(result3)
+
+//replace swops all instances of first param in string given with the second param
+let result4 = email.replace("S","m");
+console.log(result4)
+
+//template string
+const title = "Best reads of 2021"
+const author = "Mario"
+const likes = "30"
+
+//template literal creation
+//*The first method */
+let result5 = "The blog called " + title + " By:" + author + " Likes:" + likes
+console.log(result5)
+
+//*The second <method>*/ => Using backticks
+let result6 = `The blog called: ${title} By: ${author} has ${likes} likes`
+console.log(result6)
