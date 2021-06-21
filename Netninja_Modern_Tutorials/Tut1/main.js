bf64adea10738b085184ad123b4109fe8e358e19
Sachin2911 <Sachinmohan29@gmail.com>
2021-06-21 23:57:22 +0200

    Added more knowledge

--- Netninja_Modern_Tutorials/Tut1/main.js
+++ Netninja_Modern_Tutorials/Tut1/main.js
@@ -1,6 +1,25 @@
-alert("Hello world!");
-
 let age = 25;
 let year = 2021;
 
 console.log(age, year);
+
+//getting characters from strings
+let firstname = "Sachin";
+let surname = "Mohan";
+let fullName = firstname + " " + surname;
+let email = "Sachinmohan29@gmail.com"
+
+console.log(fullName)
+//for the first letter
+console.log(fullName[1])
+
+//string length -these are properties as they can be attributed to the variable
+console.log(fullName.length)
+
+//string methods - same as functions
+console.log(fullName.toUpperCase())
+console.log(fullName.toLowerCase())
+
+let index = email.indexOf("@");
+console.log(index)
+
---
+++ Netninja_Modern_Tutorials/Tut1/main2.js
@@ -0,0 +1,9 @@
+//common string methods
+
+let email = "Sachinmohan29@gmail.com";
+//Finds the last index of that chracter => where it appears last
+let result = email.lastIndexOf("a");
+console.log(result)
+
+let result2 = email.slice(6, 11)
+console.log(result2)
